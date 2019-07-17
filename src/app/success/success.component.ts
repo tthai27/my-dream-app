51b9a0a6a4b47bb566b219aea781e69d42b721b5
Tony <tthai27@gmail.com>
2019-07-16 21:11:49 -0400

    saving

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-success',
+  selector: '.app-success',
   templateUrl: './success.component.html',
   styleUrls: ['./success.component.css']
 })
